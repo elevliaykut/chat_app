b6365afd61fc12dc0723142af9d178f0d2c8a2c1
Aykut Elevli <elevli.aykut.28@gmail.com>
2025-05-12 22:24:06 +0300

    fix

--- app/Http/Controllers/Match/MatchUserController.php
+++ app/Http/Controllers/Match/MatchUserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use App\Services\User\UserService;
+use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -25,23 +26,46 @@ class MatchUserController extends Controller
     }
 
     /**
-     * Undocumented function
-     *
-     * @return void
+     * @return JsonResponse
      */
-    public function matches()
+    public function matches(): JsonResponse
     {
         $user = auth()->user();
 
         $matches = QueryBuilder::for(User::class)
             ->allowedFilters([
-                    AllowedFilter::exact('id'),
-                    AllowedFilter::exact('age')
-            ])
+                AllowedFilter::exact('id'),
+                AllowedFilter::exact('creator_user_id'),
+                AllowedFilter::exact('status'),
+                AllowedFilter::scope('near_users'),
+                AllowedFilter::scope('born_today_date'),
+                AllowedFilter::exact('gender'),
+                AllowedFilter::scope('starts_between'),
+                AllowedFilter::scope('username'),
+                AllowedFilter::scope('min_age_range'),
+                AllowedFilter::scope('max_age_range'),
+                AllowedFilter::scope('min_tall'),
+                AllowedFilter::scope('max_tall'),
+                AllowedFilter::scope('min_weight'),
+                AllowedFilter::scope('max_weight'),
+                AllowedFilter::scope('city_id'),
+                AllowedFilter::scope('job'),
+                AllowedFilter::scope('marital_status'),
+                AllowedFilter::scope('have_a_child'),
+                AllowedFilter::scope('use_cigarette'),
+                AllowedFilter::scope('use_alcohol'),
+                AllowedFilter::scope('education'),
+                AllowedFilter::scope('salary'),
+                AllowedFilter::scope('physical'),
+                AllowedFilter::scope('physical'),
+                AllowedFilter::scope('has_photos'),
+                AllowedFilter::scope('head_craft'),
+            ])  
+            ->where('id', '!=', auth()->user()->id)
             ->where('gender', $user->gender === 1 ? 0 : 1)
-            ->defaultSort('-created_at')
-            ->paginate(20);
-        
-        return API::success()->response(UserResource::collection($matches));
+            ->inRandomOrder()
+            ->first();
+
+        return API::success()->response(UserResource::make($matches));
     }
 }
